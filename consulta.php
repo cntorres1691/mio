d5b502348eb6542707fd3918fbc395f866aa3678
cntorres1691 <clao91torrestorres@gmal.com>
2016-09-06 00:39:44 -0500

    todo

--- consulta.php
+++ consulta.php
@@ -19,50 +19,50 @@ ini_set('display_errors', 1);
 	 <body class="fondo" id="page-top">
 	 <a href="login.html">+ACCESO</a>     
 	 <div class="container">
-       <table border="1" cellpadding="2" cellspacing="4" >
+        <table>
           <?php
           
             foreach ($UsuarioObj->consultarUsuario() as $c) {
                 ?>
                     
                      <tr>
-                      <td> <?php echo $c->getId(); ?> </td>
-                       <td> <?php echo $c->getUsuario(); ?> </td>
-					   <td> <?php echo $c->getPassword(); ?> </td>
+                      <td class="celda"> <?php echo $c->getId(); ?> </td>
+                       <td class="celda"> <?php echo $c->getUsuario(); ?> </td>
+					   <td class="celda"> <?php echo $c->getPassword(); ?> </td>
 					   
 					   <?php
 						$usuario_info = $UsuarioObj->consultarUsuarioInfoPorIdUsuario($c->getId());
 		
 						?>
 						
-						<td> <?php echo $usuario_info->getNombre(); ?> </td>
-					   <td> <?php echo $usuario_info->getApellidos(); ?> </td>
-					   <td> <?php echo $usuario_info->getMail(); ?> </td>
-					   <td> <?php echo $usuario_info->getSexo(); ?> </td>					   
-					   <td> <?php echo $usuario_info->getFecha_nacimiento(); ?> </td>
-					   <td> <?php echo $usuario_info->getPais(); ?> </td>
+					   <td class="celda"> <?php echo $usuario_info->getNombre(); ?> </td>
+					   <td class="celda"> <?php echo $usuario_info->getApellidos(); ?> </td>
+					   <td class="celda"> <?php echo $usuario_info->getMail(); ?> </td>
+					   <td class="celda"> <?php echo $usuario_info->getSexo(); ?> </td>					   
+					   <td class="celda"> <?php echo $usuario_info->getFecha_nacimiento(); ?> </td>
+					   <td class="celda"> <?php echo $usuario_info->getPais(); ?> </td>
 					   
 					   <?php
 						$discapacidadInfo = $UsuarioObj->consultarDiscapacidadInfoPorId($usuario_info->getTipodiscapacidad());
 						?>
-					   <td> <?php echo $discapacidadInfo->getTipo(); ?> </td>					   
+					   <td class="celda"> <?php echo $discapacidadInfo->getTipo(); ?> </td>					   
 					   
-					   <td> <?php echo $usuario_info->getPorcentajediscapacidad(); ?> </td>
+					   <td class="celda"> <?php echo $usuario_info->getPorcentajediscapacidad(); ?> </td>
 					   
 					   <?php
 						$rol = $UsuarioObj->consultarRolePorId($usuario_info->getRole());
 						?>
-					   <td> <?php echo $rol->getNombre(); ?> </td>
+					   <td class="celda"> <?php echo $rol->getNombre(); ?> </td>
 					   
-                       <td><a href="editarUsuario.php?id=<?php echo $c->getId();?>"> Editar</a>  </td>
-                       <td><a href="eliminar.php?id=<?php echo $c->getId();?>"> Eliminar</a>  </td>
+                       <td><a class="link" href="editarUsuario.php?id=<?php echo $c->getId();?>"> Editar</a>  </td>
+                       <td><a class="link" href="eliminar.php?id=<?php echo $c->getId();?>"> Eliminar</a>  </td>
                      </tr>
                    <?php
             }
             ?>
           
           <tr>
-            <td colspan=4><a href="index.php">Crear Usuario</a></td>
+            <td colspan=14><a class="link" href="index.php">Crear Usuario</a></td>
           </tr>
           
 </table>
